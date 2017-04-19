1f53da534f6a0a351c92fc22f7d57f5e0fb62b1d
Tiancheng <ltiancheng1214@126.com>
2017-04-19 19:02:02 +0800

    update C++

---
+++ Project1/Project1/TdCommander.cpp
@@ -0,0 +1,110 @@
+#include "TdCommander.h"
+#include "TdHolder.h"
+#include "ThostFtdcUserApiStruct.h"
+#include "ThostFtdcTraderApi.h"
+#include <iostream>
+#include "Config.h"
+#include <sstream>
+using namespace std;
+
+TdCommander::TdCommander(TdHolder * tdHolder, GatewayManager * gatewayManager, const std::string& destStr, bool useTopic = false){
+	this->tdHolder = tdHolder;
+	this->useTopic = useTopic;
+	this->destStr = destStr;
+	this->gatewayManager = gatewayManager;
+}
+
+TdCommander::~TdCommander(){
+	cleanup();
+}
+
+void TdCommander::close(){
+	this->cleanup();
+}
+
+void TdCommander::registerSelf(){
+	try {
+		gatewayManager->registerListener(this, this->destStr, this->useTopic);
+	}
+	catch (CMSException& e) {
+		e.printStackTrace();
+	}
+}
+
+void TdCommander::onMessage(const Message* message) {
+	try {
+		const TextMessage* textMessage = dynamic_cast<const TextMessage*> (message);
+		string text = "";
+
+		if (textMessage != NULL) {
+			text = textMessage->getText();
+		}
+		else {
+			text = "NOT A TEXTMESSAGE!";
+		}
+		printf("Contract Command Received: %s\n", text.c_str());
+		int count = 0;
+		char ** commandArray = getContractArray(text, SEPERATER, count);
+
+		CThostFtdcInputOrderField *orderField = new CThostFtdcInputOrderField();
+		strcpy(orderField->BrokerID, TD_BROKER_ID);
+		strcpy(orderField->InvestorID, INVESTOR_ID);
+		strcpy(orderField->InstrumentID, commandArray[2]);
+		if (commandArray[4] == MARKET_PRICE){
+			orderField->OrderPriceType = THOST_FTDC_OPT_AnyPrice;		//市价
+			orderField->LimitPrice = 0;									//价格
+		}
+		else{
+			orderField->OrderPriceType = THOST_FTDC_OPT_LimitPrice;		//限价
+			orderField->LimitPrice = atof(commandArray[3]);
+		}
+		if (commandArray[0] == OPEN_LONG || commandArray[0] == CLOSE_SHORT){
+			orderField->Direction = THOST_FTDC_D_Buy;					//买 
+		}
+		else{
+			orderField->Direction = THOST_FTDC_D_Sell;					//卖
+		}
+		if (commandArray[0] == OPEN_LONG || commandArray[0] == OPEN_SHORT){
+			orderField->CombOffsetFlag[0] = THOST_FTDC_OF_Open;				//开仓
+		}
+		else{
+			orderField->CombOffsetFlag[0] = THOST_FTDC_OF_Close;		//测试环境都用平今
+		}
+		orderField->VolumeTotalOriginal = stoi(commandArray[1]);		//数量
+		//以下是固定的字段
+		orderField->CombHedgeFlag[0] = THOST_FTDC_HF_Speculation;		//投机 
+		orderField->TimeCondition = THOST_FTDC_TC_GFD;				//当日有效 '3'
+		orderField->VolumeCondition = THOST_FTDC_VC_AV;				//任何数量 '1'
+		orderField->MinVolume = 1;
+		orderField->ContingentCondition = THOST_FTDC_CC_Immediately;	//立即触发'1'
+		orderField->ForceCloseReason = THOST_FTDC_FCC_NotForceClose;	//非强平 '0'
+		orderField->IsAutoSuspend = 0;
+		orderField->UserForceClose = 0;
+
+		this->tdHolder->tdApi->ReqOrderInsert(orderField, 1000);
+
+		delete commandArray;
+		delete orderField;
+	}
+	catch (CMSException& e) {
+		e.printStackTrace();
+	}
+}
+
+char ** getContractArray(string& command, const char& seperator, int& count){
+	count = std::count(command.begin(), command.end(), seperator) + 1;
+	char ** commandArray = new char*[count];
+	std::istringstream ssin(command);
+	string cmd;
+	int i = 0;
+	while (getline(ssin, cmd, seperator)){
+		commandArray[i] = new char[cmd.length() + 1];
+		strcpy(commandArray[i], cmd.c_str());
+		i++;
+	}
+	return commandArray;
+}
+
+void TdCommander::cleanup(){
+
+}
---
+++ Project1/Project1/TdCommander.h
@@ -0,0 +1,50 @@
+#ifndef TDCOMMANDER_H
+#define TDCOMMANDER_H
+
+#include "ThostFtdcTraderApi.h"
+#include "ThostFtdcUserApiStruct.h"
+#include "TdHolder.h"
+#include "GatewayManager.h"
+#include <activemq/library/ActiveMQCPP.h>
+#include <decaf/lang/Thread.h>
+#include <decaf/lang/Runnable.h>
+#include <decaf/util/concurrent/CountDownLatch.h>
+#include <decaf/lang/Integer.h>
+#include <decaf/lang/Long.h>
+#include <decaf/lang/System.h>
+#include <activemq/core/ActiveMQConnectionFactory.h>
+#include <activemq/util/Config.h>
+#include <cms/Connection.h>
+#include <cms/Session.h>
+#include <cms/TextMessage.h>
+#include <cms/BytesMessage.h>
+#include <cms/MapMessage.h>
+#include <cms/ExceptionListener.h>
+#include <cms/MessageListener.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <memory>
+
+using namespace cms;
+using namespace decaf::lang;
+using namespace decaf::util::concurrent;
+
+///从queue读取有关Trade的命令(目前只有提交订单)，并且执行
+class TdCommander : public MessageListener{
+public:
+	TdCommander(TdHolder * tdHolder, GatewayManager * gatewayManager, const std::string& destStr, bool useTopic = false);
+	virtual ~TdCommander();
+	void close();
+	void registerSelf();
+	///命令格式：OS/OL/CS/CL,4,A1709,3715,M/L(开空/开多/平空/平多,合约数量,合约标识,价格,市/限价)
+	virtual void onMessage(const Message* message);
+private:
+	TdHolder * tdHolder;
+	GatewayManager * gatewayManager;
+	bool useTopic;
+	std::string destStr;
+	void cleanup();
+};
+
+#endif
