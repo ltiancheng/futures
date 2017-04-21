ec3c1b4e865fec8820a1715e1cd5338fb166f094
Guilin D <Guilin D>
2017-04-22 01:00:56 +0800

    update command ctp impl

---
+++ futures/src/main/java/com/ltc/base/vo/FullCommandVO.java
@@ -0,0 +1,18 @@
+package com.ltc.base.vo;
+
+public class FullCommandVO {
+	private CommandVO command;
+	private ContractVO contract;
+	public CommandVO getCommand() {
+		return command;
+	}
+	public void setCommand(CommandVO command) {
+		this.command = command;
+	}
+	public ContractVO getContract() {
+		return contract;
+	}
+	public void setContract(ContractVO contract) {
+		this.contract = contract;
+	}
+}
